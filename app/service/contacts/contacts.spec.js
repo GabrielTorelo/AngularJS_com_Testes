b1bff26954a09c5f221f72fd5ec00834c2e7364b
Gabriel Torelo <57268681+GabrielTorelo@users.noreply.github.com>
2023-02-18 18:43:44 -0300

    feat: cria terceiro teste 'Lista de Contatos OK'

--- app/service/contacts/contacts.spec.js
+++ app/service/contacts/contacts.spec.js
@@ -1,6 +1,37 @@
 describe('Grupo de Testes', function() {
   let TestContacts; // Variável auxiliar
 
+  const TestContactsList = [  // Constante com Lista de Contatos - JSON
+    {
+        "id": 1,
+        "nome": "Calebe Tiago Arthur da Luz",
+        "idade": 19,
+        "email": "calebe-daluz97@gigaonline.com.br",
+        "senha": "aUdxiJwstR"
+    },
+    {
+        "id": "2",
+        "nome": "Paulo Yuri da Rosa",
+        "idade": 23,
+        "email": "paulo_yuri_darosa@br.pwc.com",
+        "senha": "c0xsjwPxJG"
+    },
+    {
+        "id": "3",
+        "nome": "Benedito Benício Gabriel Peixoto",
+        "idade": 27,
+        "email": "benedito_peixoto@arysta.com.br",
+        "senha": "ytTA2lySWu"
+    },
+    {
+        "id": "4",
+        "nome": "Diego Cláudio Oliver Cardoso",
+        "idade": 33,
+        "email": "diegoclaudiocardoso@smbcontabil.com.br",
+        "senha": "bucpOXeoK4"
+    }
+  ]
+
   // Chama o Módulo 'api.contacts'
   beforeEach(angular.mock.module('api.contacts')); /* O 'beforeEach' é um método chamado antes
                                                       de qualquer execução de teste. Ou seja,
@@ -38,6 +69,13 @@ describe('Grupo de Testes', function() {
     it('Método OK', function() {
       expect(TestContacts.all) // O método 'expect' espera e retorna uma resposta.
         .toBeDefined(); // O método 'toBeDefined' testa e retorna se o método '.all' foi definido.
+    });
+
+    // Terceiro Caso de Teste
+    it('Lista de Contatos OK', function() {
+      expect(TestContacts.all()) // O método 'expect' espera e retorna uma resposta.
+        .toEqual(TestContactsList) /* O método 'toEqual' testa se o retorno do método
+                                      '.all' é igual a constante 'TestContactsList' */
     });
     
   });
