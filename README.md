9ea33cf9ba393fc3f4e5ffc777cbcb751ebda784
Gabriel Torelo <57268681+GabrielTorelo@users.noreply.github.com>
2023-02-17 22:53:05 -0300

    Início do Projeto + README.md

---
+++ README.md
@@ -0,0 +1,47 @@
+# AngularJS com Testes
+
+Meu Primeiro Projeto usando Testes Automatizados (Jasmine e Karma) em AngularJS
+
+## Instalação
+
+Clone o Projeto usando:
+
+```bash
+  git clone https://github.com/GabrielTorelo/AngularJS_com_Testes.git
+```
+
+Vá até o diretório do projeto:
+
+```bash
+  cd angular_com_testes
+```
+
+Instale os pacotes e suas dependências:
+
+```bash
+  npm i
+```
+
+Inicie os testes:
+
+```bash
+  karma start
+```
+
+## 🛠 Pacotes Usados
+
+| Nome                  | Versão   |
+| :-------------------- | :------- |
+| karma                 | `^6.4.1` |
+| karma-cli             | `=2.0.0` |
+| jasmine-core          | `^4.5.0` |
+| karma-jasmine         | `^5.1.0` |
+| karma-chrome-launcher | `^3.1.1` |
+|||
+| angular               | `^1.8.3` |
+| angular-mocks         | `^1.8.3` |
+| angular-ui-router     | `^1.0.3` |
+
+## 🔗 Meus Links
+[![portfolio](https://img.shields.io/badge/GitHub-100000?style=for-the-badge&logo=github&logoColor=white)](https://github.com/GabrielTorelo)
+[![linkedin](https://img.shields.io/badge/linkedin-0A66C2?style=for-the-badge&logo=linkedin&logoColor=white)](https://www.linkedin.com/in/gabriel-torelo/)
---
+++ karma.conf.js
@@ -0,0 +1,59 @@
+// Configuração do Karma
+
+module.exports = function (config) {
+	config.set({
+		// Caminho base que será usado para resolver todos os padrões (por exemplo, arquivos, excluir)
+		basePath: "",
+
+		// Frameworks usados
+		frameworks: ["jasmine"],
+
+		// Lista de arquivos para carregar no navegador
+		files: [
+      'node_modules/angular/angular.js',
+      'node_modules/angular-mocks/angular-mocks.js',
+      'node_modules/angular-ui-router/release/angular--ui-router.js',
+
+      'app/service/contacts/contacts.js',
+
+      'app/app.js',
+      'app/service/contacts/*.spec.js'
+    ],
+
+		// Lista de arquivos para não carregar no navegador (excluir)
+		exclude: [],
+
+		// Pré-processar arquivos correspondentes antes de entregá-los ao navegador
+		preprocessors: {},
+
+		// Gera relatório de resultados dos testes
+		reporters: ["progress"],
+
+		// Porta do serviço WEB
+		port: 9876,
+
+		// Servidor de hospedagem
+		hostname: "localhost",
+
+		// Serviço de endereço de escuta
+		listenAddress: "localhost",
+
+		// Ativar/Desativar cores na saída (relatórios e logs)
+		colors: true,
+
+		// Nível de registro
+    logLevel: config.LOG_INFO,
+
+		// Ativar/Desativar o observador de arquivos, executando testes sempre que qualquer arquivo for alterado
+		autoWatch: true,
+
+		// Inicia esses navegadores
+		browsers: ["Chrome"],
+
+		// Modo de integração contínua, se verdadeiro, o Karma captura os navegadores, executa os testes e sai
+		singleRun: false,
+
+		// Nível de simultaneidade, quantas instâncias do navegador devem ser iniciadas simultaneamente
+		concurrency: Infinity,
+	})
+}
